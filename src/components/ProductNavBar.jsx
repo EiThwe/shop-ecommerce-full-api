fc59d442c6dd48a51001df00a7317be9509db74f
Ei Thwe <thwethwehtun2000.gmail.com>
2023-04-14 09:56:31 +0630

    custom scrollbar

--- src/components/ProductNavBar.jsx
+++ src/components/ProductNavBar.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import {IoIosArrowDown} from "react-icons/io"
+import React from "react";
+import { IoIosArrowDown } from "react-icons/io";
+import { shopCategories } from "../utils/data";
 
 const ProductNavBar = () => {
   return (
-   <nav className=' bg-white md:px-10 py-10 sm:px-5 px-1 shadow-sm uppercase font-bold w-full'>
-    <div className="flex sm:flex-row flex-col sm:gap-0 gap-3 justify-between items-center w-full">
-        <div className="sm:flex grid grid-cols-4 sm:gap-5 gap-2 sm:text-xs text-[10px]  text-gray-400 w-full ">
-            <div className='flex justify-center items-center'><a href="#" className='hover:text-gray-800'>All</a></div>
-            <div className='flex justify-center items-center'><a href="#" className='hover:text-gray-800'>Home decor</a></div>
-            <div className='flex justify-center items-center'><a href="#" className='hover:text-gray-800'>Lighting</a></div>
-            <div className='flex justify-center items-center'><a href="#" className='hover:text-gray-800'>Decoration</a></div>
-            <div className='flex justify-center items-center col-span-2'><a href="#" className='hover:text-gray-800 '>Vases</a></div>
-            <div className='flex justify-center items-center col-span-2'><a href="#" className='hover:text-gray-800 '>Basics</a></div>
-         </div>
-         <div className='flex gap-10 sm:text-xs text-[10px] text-gray-800'>
-            <span className='hover:cursor-pointer flex items-center gap-2'>filter<IoIosArrowDown/></span>
-         </div>
-    </div>
-   </nav>
-  )
-}
+    <nav className=" bg-white px-10 py-10 shadow-sm uppercase font-bold w-full sm:overflow-x-hidden scrollbar-thin scrollbar-thumb-gray-900 scrollbar-track-gray-100 overflow-x-scroll">
+      <div className="flex sm:gap-5 gap-10 justify-between items-center w-auto flex-nowrap">
+        <div className="flex sm:gap-5 gap-10 sm:text-xs text-[10px]  text-gray-400 flex-nowrap">
+          {shopCategories.map((category, index) => (
+            <a
+              href="#"
+              className="hover:text-gray-800 whitespace-nowrap"
+              key={index}
+            >
+              {category.name}
+            </a>
+          ))}
+        </div>
+        <div className="flex gap-10 sm:text-xs text-[10px] text-gray-800 sm:pr-0 pr-10">
+          <span className="hover:cursor-pointer flex items-center gap-2">
+            filter
+            <IoIosArrowDown />
+          </span>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default ProductNavBar
+export default ProductNavBar;
